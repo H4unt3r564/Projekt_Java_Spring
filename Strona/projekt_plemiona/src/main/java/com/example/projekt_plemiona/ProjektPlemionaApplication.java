1698e0ca2a250113471ddc8d45d4028e48508fc1
H4unt3r564 <jasiekpraga@gmail.com>
2026-05-16 18:13:37 +0200

    pierwsze rzeczy
    Dodanie prostego logowania, rejestracji i polaczenai z baza

---
+++ Strona/projekt_plemiona/src/main/java/com/example/projekt_plemiona/ProjektPlemionaApplication.java
@@ -0,0 +1,14 @@
+package com.example.projekt_plemiona;
+
+import com.example.projekt_plemiona.services.CustomUserDetailsService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPlemionaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektPlemionaApplication.class, args);
+    }
+
+}
---
+++ Strona/projekt_plemiona/src/main/java/com/example/projekt_plemiona/repositories/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.example.projekt_plemiona.repositories;
+
+import com.example.projekt_plemiona.models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    Optional<Player> findByUsername(String username);
+}
