917b14375dbd3f5b1bf9f11a17903edaf9b258e5
yourst4r4 <ania.kalinowska.bialystok@gmail.com>
2026-06-09 02:28:25 +0200

    mapaaaaa i nowy css i wglll swiecaca na rozowo pinezkaaaa ;ppp

--- Strona/projekt_plemiona/src/main/java/com/example/projekt_plemiona/controllers/MapController.java
+++ Strona/projekt_plemiona/src/main/java/com/example/projekt_plemiona/controllers/MapController.java
@@ -9,6 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.projekt_plemiona.models.Player;
+import com.example.projekt_plemiona.models.Village;
+import com.example.projekt_plemiona.repositories.VillageRepository;
+import com.example.projekt_plemiona.services.UserService;
+
 import java.util.List;
 
 @Controller
@@ -16,22 +21,50 @@ public class MapController {
 
     private final VillageMapRepository villageMapRepository;
     private final MapService mapService;
+    private final UserService userService;
+    private final VillageRepository villageRepository;
 
     public MapController(
             VillageMapRepository villageMapRepository,
-            MapService mapService
+            MapService mapService,
+            UserService userService,
+            VillageRepository villageRepository
     ) {
         this.villageMapRepository = villageMapRepository;
         this.mapService = mapService;
+        this.userService = userService;
+        this.villageRepository = villageRepository;
     }
 
     @GetMapping("/map")
     public String map(Model model) {
 
+        Player player =
+                userService.getCurrentPlayer();
+
+        Village village =
+                villageRepository
+                        .findFirstByPlayer_PlayerId(
+                                player.getPlayerId())
+                        .orElse(null);
+
         model.addAttribute(
                 "villages",
                 mapService.getVillagesForMap()
         );
+
+        if(village != null) {
+
+            model.addAttribute(
+                    "myVillageX",
+                    village.getCoordinateX()
+            );
+
+            model.addAttribute(
+                    "myVillageY",
+                    village.getCoordinateY()
+            );
+        }
 
         return "map";
     }
